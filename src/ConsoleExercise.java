d520b2c3c0f76ded9f61e932d086fc1c2ca054c4
Stephen Guedea <guedea.stephen@gmail.com>
2018-11-07 16:02:12 -0600

    Updated ConsoleExercise

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -31,9 +31,23 @@ public class ConsoleExercise {
 //        2. Prompt a user to enter 3 words and store each of them in a separate variable,
 //        then display them back, each on a newline.
 
-//        System.out.println("Please enter your favorite superheros: ");
+//        System.out.println("Please enter 3 favorite superheros: ");
 //        String hero = scanner.nextLine();
 //        System.out.println(hero);
+
+        // BETTER WAY
+
+
+//        System.out.println("Please enter 3 favorite superheros: ");
+//        String hero = scanner.nextLine();
+//        System.out.println(hero);
+//
+//        String hero1 = scanner.next();
+//        String hero2 = scanner.next();
+//        String hero3 = scanner.next();
+//
+//
+//        System.out.printf("%s %s %s", hero1, hero2, hero3);
 
 //        3. Prompt a user to enter a sentence, then store that sentence in a String variable using the .next method,
 //        then display that sentence back to the user.
@@ -50,17 +64,35 @@ public class ConsoleExercise {
 
 
 //        CALCULATE THE PERIMETER AND AREA and VOLUME (of a rectangle)
-        System.out.println("Enter the length and width:");
-        float num1 = scanner.nextFloat();
-        float num2 = scanner.nextFloat();
+//        System.out.println("Enter the length and width:");
+//        float num1 = scanner.nextFloat();
+//        float num2 = scanner.nextFloat();
+//
+//        System.out.println("The perimeter is " + (2*num1 + 2*num2));
+//        scanner.nextLine();
+//        System.out.printf("The area is %.1f", (num1 * num2));
+//
+//        System.out.println("\nEnter the base: ");
+//        float num3 = scanner.nextFloat();
+//        System.out.printf("The volume is %.2f",(num1 * num2 * num3));
 
-        System.out.println("The perimeter is " + (2*num1 + 2*num2));
-        scanner.nextLine();
-        System.out.printf("The area is %.1f", (num1 * num2));
+        System.out.print("Enter the length of the classroom in feet: ");
+        int lengthInFeet = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("\nEnter the base: ");
-        float num3 = scanner.nextFloat();
-        System.out.printf("The volume is %.2f",(num1 * num2 * num3));
+        System.out.print("Enter the width of the classroom in feet: ");
+        int widthInFeet = Integer.parseInt(scanner.nextLine());
+
+        int area = lengthInFeet * widthInFeet;
+        int perimeter = lengthInFeet * 2 + widthInFeet * 2;
+        String message = String.format("Room area is %d feet\nPerimeter is %d feet", area, perimeter);
+
+        System.out.println(message);
+
+
+
+
+
+
 
 
     }
