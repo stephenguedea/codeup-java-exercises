481b430e5f12fabff3ea5c5a96381a8db322785f
Stephen Guedea <guedea.stephen@gmail.com>
2018-11-08 21:51:06 -0600

    Updating Java String

--- src/Bob.java
+++ src/Bob.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String talkToBob;
+//        String talkToBob;
         System.out.println("Talk to Bob!");
         do {
 
